b02df5c4d2baba4927d611e9113fa1966168d042
Kuba Oniszczuk <j.oniszczuk@gmail.com>
2019-01-29 16:25:33 +0100

    setup complited

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
-console.log('It works.');
+new Vue({
+  el: "#app",
+  data: {
+    total: 0,
+    products: [
+      { title: "product 1" },
+      { title: "product 2" },
+      { title: "product 3" }
+    ],
+    cart: {}
+  },
+  methods: {
+    addToCart: function() {
+      this.total += 9.99;
+    }
+  }
+})
